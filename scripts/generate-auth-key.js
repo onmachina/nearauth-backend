ae9f24c79ed7d3805dfc26efbb8b9d6107b0cc9c
Alexander Fadeev <fadeevab.com@gmail.com>
2023-02-15 20:20:45 +0200

    Introduce a script to generate `x-auth-key` value
    Usage:
    `npm run auth -- alice.testnet`
    or
    `node scripts/generate-auth-key.js alice.near --networkId mainnet`

---
+++ scripts/generate-auth-key.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+'use strict';
+const nearAPI = require('near-api-js');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const generate_auth_token = async (accountId, networkId) => {
+  const nodeUrl = `https://rpc.${networkId}.near.org`;
+  const keyFileName = `${require('os').homedir()}/.near-credentials/${networkId}/${accountId}.json`;
+
+  let keyFile;
+
+  try {
+    keyFile = require(keyFileName);
+  } catch (err) {
+    console.error(
+      `Account ${accountId} is not found for the ${networkId}. Please, login with near CLI and try again.`
+    );
+    console.error(`File not found: ${keyFileName}.`);
+    process.exit(1);
+  }
+
+  const privKey = nearAPI.utils.KeyPair.fromString(
+    keyFile.secret_key || keyFile.private_key
+  );
+
+  const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+  keyStore.setKey(networkId, accountId, privKey);
+
+  const near = await nearAPI.connect({ keyStore, networkId, nodeUrl });
+
+  const signer = near.connection.signer;
+
+  const signerPublicKey = (
+    await signer.getPublicKey(accountId, networkId)
+  ).toString();
+
+  const account = new nearAPI.Account(near.connection, accountId);
+
+  const nonce = (await account.getAccessKeys())
+    .find((k) => k.public_key === signerPublicKey)
+    .access_key.nonce.toString();
+
+  const accountData = Buffer.from(JSON.stringify({ id: accountId, nonce }));
+  const signatureData = await signer.signMessage(
+    accountData,
+    accountId,
+    networkId
+  );
+
+  const publicKey = Buffer.from(signatureData.publicKey.toString());
+  const signature = Buffer.from(signatureData.signature);
+
+  const credentials = Buffer.from(
+    JSON.stringify({
+      account: accountData.toString('base64'),
+      publicKey: publicKey.toString('base64'),
+      signature: signature.toString('base64'),
+    })
+  ).toString('base64');
+
+  console.log(credentials);
+};
+
+(async function () {
+  yargs(hideBin(process.argv))
+    .usage('Usage: $0 <accountId> [--networkId <mainnet|testnet>]')
+    .command(
+      '$0 <accountId>',
+      'generate an authentication JWT token',
+      (yargs) => {
+        return yargs.positional('accountId', {
+          describe: 'NEAR account identifier',
+          type: 'string',
+        });
+      },
+      async (argv) => {
+        await generate_auth_token(argv.accountId, argv.networkId);
+      }
+    )
+    .option('networkId', {
+      describe: 'NEAR network',
+      choices: ['testnet', 'mainnet'],
+      default: 'testnet',
+    })
+    .parse();
+})();
