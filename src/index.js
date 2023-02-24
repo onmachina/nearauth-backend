578f3987b8baa4ad81a8135e27ba0be915879604
Alexander Fadeev <fadeevab.com@gmail.com>
2023-02-24 14:09:15 +0200

    Enable CORS: allow '*' for demo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 require('express-async-errors');
 
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cors());
 app.use('/auth/v1', auth);
 app.use('/auth/v1.0', auth);
 app.use(notFound);
