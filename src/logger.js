db8b1a9af9694daa7d5171393493b03a2f97c0b1
Alexander Fadeev <fadeevab.com@gmail.com>
2023-05-19 15:51:24 +0300

    Show timestamp in the logs

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,16 @@
 const winston = require('winston');
 const env = require('./env');
 
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
 module.exports = winston.createLogger({
   level: env.LOG_LEVEL,
   transports: [new winston.transports.Console()],
   format: winston.format.combine(
     winston.format.colorize(),
-    winston.format.simple()
+    winston.format.timestamp(),
+    myFormat
   ),
 });
