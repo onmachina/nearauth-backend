328f7c0873cd9e4346c65a53c30dc49920fa7d02
Alexander Fadeev <fadeevab.com@gmail.com>
2024-02-27 19:40:00 +0200

    Another default localhost port for `jwt.audience`

--- src/env.js
+++ src/env.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
+const DEFAULT_PORT = 5000;
+
 const env = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   NEAR_ENV: process.env.NEAR_ENV || 'sandbox', // 'testnet', 'mainnet'
-  SERVER_PORT: process.env.SERVER_PORT || 5000,
-  SERVER_STORAGE_DOMAIN: process.env.SERVER_STORAGE_DOMAIN || 'localhost:5000',
+  SERVER_PORT: process.env.SERVER_PORT || DEFAULT_PORT,
   PRIVATE_KEY_PATH:
     process.env.PRIVATE_KEY_PATH || 'test/prime256v1-private-key.pem',
-  ISSUER: process.env.ISSUER || 'http://localhost',
-  AUDIENCE: process.env.AUDIENCE || 'http://localhost',
+  ISSUER: process.env.ISSUER || `http://localhost:${DEFAULT_PORT}/`,
+  AUDIENCE: process.env.AUDIENCE || 'http://localhost:5001/',
+  SERVER_STORAGE_DOMAIN: process.env.SERVER_STORAGE_DOMAIN || 'localhost:5002',
 };
 
 module.exports = env;
